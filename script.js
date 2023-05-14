957fd4c92e62518fc1d79528dfa92bed0bd02fb6
Chris Olmedo <cbo238@nyu.edu>
2023-05-13 21:50:01 -0400

    final commit

---
+++ script.js
@@ -0,0 +1,312 @@
+let portfolioCollection = [
+	{
+		"id" : "donut",
+		"itemTitle" : "Donut w/Coffee",
+		"description" : "This is a strawberry donut with coffee that I modeled in Blender. This was my first model ever and was inspired by Andrew Price's donut and coffee youtube tutorial.",
+		"imageArray" : ["3D Portfolio/Renders/Donut Renders/First Real Picture.png"],
+		"descriptArray" : ["Strawberry Donut w/Coffee"],
+		"animation" : ["3D Portfolio/Animations/Donut Animation (Motion Blur).mp4"],
+		"backgroundColor" : "rgb(176,139,164,0.5)",
+		"textColor" : ""
+	},
+	{
+		"id" : "chair",
+		"itemTitle" : "Wooden Chair",
+		"description" : "This is a wooden chair that I modeled in Blender. This was my second ever model and was inspired by the Andrew Price's chair modeling youtube tutorial.",
+		"imageArray" : ["3D Portfolio/Renders/Wood Chair Renders/Wood Chair (Final).png"],
+		"descriptArray" : ["Wooden Chair Studio View"],
+		"animation" : ["3D Portfolio/Animations/Chair Animation.mp4"],
+		"backgroundColor" : "rgb(164,164,201,0.5)",
+		"textColor" : ""
+	},
+	{
+		"id" : "chess",
+		"itemTitle" : "Wooden Chess Set",
+		"description" : "This is a wooden chess set that I modeled in Blender. I tried to model it in a similar style to the set I have at my house from piece shape to wood color and texture. Hope you enjoy the following renders!",
+		"imageArray" : ["3D Portfolio/Renders/Chess Set Renders/Chess Set (Final 1k).png",
+						"3D Portfolio/Renders/Chess Set Renders/View 1.png",
+						"3D Portfolio/Renders/Chess Set Renders/View 2.png",
+						"3D Portfolio/Renders/Chess Set Renders/View 3.png",
+						"3D Portfolio/Renders/Chess Set Renders/View 4.png",
+						"3D Portfolio/Renders/Chess Set Renders/View 5.png"],
+		"descriptArray" : ["Whole Chess Set",
+						   "Black Pieces Perspective View",
+						   "Chess Board and Pieces Mirror Reflection",
+						   "White Pieces Perspective View",
+						   "White King in Spotlight w/Shadow",
+						   "Board View of Black Pieces"],
+		"animation" : ["3D Portfolio/Animations/Chess Set Animation.mp4"],
+		"backgroundColor" : "rgb(231,212,189,0.5)",
+		"textColor" : ""
+	},
+	{
+		"id" : "ring",
+		"itemTitle" : "Rose Gold Ring",
+		"description" : "This is a rose gold ring that I modeled in Blender. I based the model off of a ring I saw at a jewelry shop in the city. I was able to take some pictures of the actual ring for reference.",
+		"imageArray" : ["3D Portfolio/Renders/Ring Renders/Ring (DOF 1).png",
+						"3D Portfolio/Renders/Ring Renders/Ring (DOF 2).png",
+						"3D Portfolio/Renders/Ring Renders/Ring (DOF 3).png",
+						"3D Portfolio/Renders/Ring Renders/Ring (DOF 4).png"],
+		"descriptArray" : ["Isometric View",
+						   "Layed Down Ring",
+						   "Front View From Far",
+						   "Close Up Top View"],
+		"animation" : ["3D Portfolio/Animations/Ring Animation.mp4"],
+		"backgroundColor" : "rgb(174,176,186,0.5)",
+		"textColor" : ""
+	},
+	{
+		"id" : "dogge",
+		"itemTitle" : "Dogge Walking",
+		"description" : "This is a Dogge pixel art that I turned 3D by modeling into Blender. I based the model and background off of a Dogge pixel art image that my younger brother Lucas made. It's really cute and I was happy with how it came out.",
+		"imageArray" : ["3D Portfolio/Renders/Dogge Renders/Dogge (Final).jpg"],
+		"descriptArray" : ["Dogge Walking Still"],
+		"animation" : ["3D Portfolio/Animations/Dogge Animation.mp4"],
+		"backgroundColor" : "rgb(188,208,192,0.5)",
+		"textColor" : ""
+	},
+	{
+		"id" : "sword",
+		"itemTitle" : "Blue Medieval Sword",
+		"description" : "This is a Medieval Style Sword that I modeled in Blender. I based the model off of a fantacy reference image I found online. It had a really nice grain detail on the steel, wrapping on the handle, and a sort of magical glow eminating from the edge.",
+		"imageArray" : ["3D Portfolio/Renders/Sword Renders/Sword (Final).png",
+						"3D Portfolio/Renders/Sword Renders/Sword (DOF 1).png",
+						"3D Portfolio/Renders/Sword Renders/Sword (DOF 2).png",
+						"3D Portfolio/Renders/Sword Renders/Sword (DOF 3).png",
+						"3D Portfolio/Renders/Sword Renders/Sword (DOF 4).png"],
+		"descriptArray" : ["Entire View of Sword",
+						   "Close Up From Blade",
+						   "Entire View of Blade",
+						   "Close Up From Hilt",
+						   "Only The Handle"],
+		"animation" : ["3D Portfolio/Animations/Sword Animation.mp4"],
+		"backgroundColor" : "rgb(155,179,198,0.5)",
+		"textColor" : "#FFFFFF"
+	},
+	{
+		"id" : "bat",
+		"itemTitle" : "Batarang Collection",
+		"description" : "This is a Medieval Style Sword that I modeled in Blender. I based the model off of a fantacy reference image I found online. It had a really nice grain detail on the steel, wrapping on the handle, and a sort of magical glow eminating from the edge.",
+		"imageArray" : ["3D Portfolio/Renders/Batarang Renders/Batarang (Archam Wall).png",
+						"3D Portfolio/Renders/Batarang Renders/Batarang (Beyond Wall).png",
+						"3D Portfolio/Renders/Batarang Renders/Batarang (Dark Night Wall).png",
+						"3D Portfolio/Renders/Batarang Renders/Batarang (DC Wall).png"],
+		"descriptArray" : ["Archam Night Batarang",
+						   "Batman Beyond Batarang",
+						   "The Dark Night Batarang",
+						   "DC Bataman Batarang"],
+		"animation" : ["3D Portfolio/Animations/Batarang Animation.mp4"],
+		"backgroundColor" : "rgb(52,52,52,0.5)",
+		"textColor" : "#FFFFFF"
+	},
+	{
+		"id" : "x-wing",
+		"itemTitle" : "Low Polly X-Wing Fighter",
+		"description" : "This is a Medieval Style Sword that I modeled in Blender. I based the model off of a fantacy reference image I found online. It had a really nice grain detail on the steel, wrapping on the handle, and a sort of magical glow eminating from the edge.",
+		"imageArray" : ["3D Portfolio/Renders/X-Wing Renders/X-Wing (Landed Final).png",
+						"3D Portfolio/Renders/X-Wing Renders/X-Wing (Landed w_R2).png",
+						"3D Portfolio/Renders/X-Wing Renders/X-Wing and TIE (Square).png",
+						"3D Portfolio/Renders/X-Wing Renders/X-Wing (Closed).png",
+						"3D Portfolio/Renders/X-Wing Renders/X-Wing (Third).png"],
+		"descriptArray" : ["X-Wing Landed in Hanger",
+						   "X-Wing Landed w/R2-D2",
+						   "X-Wing and TIE Fighter",
+						   "X-Wing Flying w/Closed Wings",
+						   "X-Wing Flying w/Open Wings"],
+		"animation" : ["3D Portfolio/Animations/X-Wing Animation.mp4"],
+		"backgroundColor" : "rgb(6,5,3,0.5)",
+		"textColor" : "#FFFFFF"
+	},
+	{
+		"id" : "lightsaber",
+		"itemTitle" : "Luke's Lightsaber (ROTJ)",
+		"description" : "This is a Medieval Style Sword that I modeled in Blender. I based the model off of a fantacy reference image I found online. It had a really nice grain detail on the steel, wrapping on the handle, and a sort of magical glow eminating from the edge.",
+		"imageArray" : ["3D Portfolio/Renders/ROTJ Lightsaber Renders/ROTJ Lightsaber (Eevee).png",
+						"3D Portfolio/Renders/ROTJ Lightsaber Renders/ROTJ Lightsaber (Second).png"],
+		"descriptArray" : ["Fully Activated Lightsaber",
+						   "Lightsaber Hilt Close Up"],
+		"animation" : [],
+		"backgroundColor" : "rgb(6,5,3,0.5)",
+		"textColor" : "#FFFFFF"
+	},
+	{
+		"id" : "jar",
+		"itemTitle" : "Jam Session Jam Jar",
+		"description" : "This is a Medieval Style Sword that I modeled in Blender. I based the model off of a fantacy reference image I found online. It had a really nice grain detail on the steel, wrapping on the handle, and a sort of magical glow eminating from the edge.",
+		"imageArray" : ["3D Portfolio/Renders/Jam Session Jar/Jam1.jpg",
+						"3D Portfolio/Renders/Jam Session Jar/Logo.jpg"],
+		"descriptArray" : ["Jam Session Sample Jar",
+						   "Jam Session Logo"],
+		"animation" : [],
+		"backgroundColor" : "rgb(147,34,46,0.5)",
+		"textColor" : "#FFFFFF"
+	}
+];
+
+let projectTitleElem;
+let projectDescElem;
+let displayGridElem;
+let projectDispElem; 
+
+document.addEventListener("DOMContentLoaded", function() {
+	projectTitleElem = document.getElementById("projectTitle");
+	projectDescElem = document.getElementById("projectDescript");
+	displayGridElem = document.getElementById("displayGrid");
+	projectDispElem = document.getElementById("projectDisplay");
+
+	let queryString = window.location.search;
+	let urlParams = new URLSearchParams(queryString);
+	let urlSection = urlParams.get('section');
+	let urlID = urlParams.get('id');
+
+	if (urlSection != "item") {
+		if (urlSection == "animations") {
+		  	projectTitleElem.innerText = "Project Animations:";
+		}
+		else if (urlSection == "renders") {
+		  	projectTitleElem.innerText = "Project Renders:";
+		}
+
+		/* Create thumbnails for matching category, or all */
+		for (let i = 0; i < portfolioCollection.length; i++) {
+			if (urlSection == "" || urlSection == null){
+				if (portfolioCollection[i]["animation"].length >= 1) {
+					createAnimationPreview(portfolioCollection[i]);
+				}
+				createRenderPreview(portfolioCollection[i]);
+			}
+			else if (urlSection == "animations") {
+				createAnimationPreview(portfolioCollection[i]);
+			}
+			else if (urlSection == "renders") {
+				createRenderPreview(portfolioCollection[i]);
+			}
+		}
+	}
+
+	else {
+		for (let i = 0; i < portfolioCollection.length; i++) {
+			if ((portfolioCollection[i]["id"]+"Animation") == urlID) {
+				createAniProjectPage(portfolioCollection[i]);
+			}
+			else if ((portfolioCollection[i]["id"]+"Render") == urlID) {
+				createRenProjectPage(portfolioCollection[i]);
+			}
+		}
+	}
+});
+
+function createAnimationPreview(projCollection) {
+	let newPreviewLink = document.createElement("A");
+	newPreviewLink.href = "index.html?section=item&id=" + projCollection["id"] + "Animation";
+
+	let newPreviewDiv = document.createElement("DIV");
+	newPreviewDiv.setAttribute("id","video-box");
+	newPreviewDiv.style.backgroundColor = projCollection["backgroundColor"];
+	newPreviewLink.appendChild(newPreviewDiv);
+
+	let newPreviewVid = document.createElement("VIDEO");
+	let newPreviewSource = document.createElement("SOURCE");
+	newPreviewSource.src = projCollection["animation"][0];
+	newPreviewSource.type = "video/mp4";
+	newPreviewVid.autoplay = true;
+	newPreviewVid.loop = true;
+	newPreviewVid.muted = true;
+	newPreviewVid.appendChild(newPreviewSource);
+	newPreviewDiv.appendChild(newPreviewVid);
+
+	let newPreTitleDiv = document.createElement("DIV");
+	newPreTitleDiv.setAttribute("id","image-descript");
+	newPreTitleDiv.style.backgroundColor = projCollection["backgroundColor"];
+	let newPreTitleText = document.createElement("p");
+	newPreTitleText.setAttribute("id","img-descript");
+	if (projCollection["textColor"] != "") { newPreTitleText.style.color = projCollection["textColor"]; }
+	newPreTitleText.innerText = projCollection["itemTitle"] + " Animation";
+	newPreTitleDiv.appendChild(newPreTitleText);
+	newPreviewDiv.appendChild(newPreTitleDiv);
+
+	displayGridElem.appendChild(newPreviewLink);
+}
+
+function createRenderPreview(projCollection) {
+	let newPreviewLink = document.createElement("A");
+	newPreviewLink.href = "index.html?section=item&id=" + projCollection["id"] + "Render";
+
+	let newPreviewDiv = document.createElement("DIV");
+	newPreviewDiv.setAttribute("id","image-box");
+	newPreviewDiv.style.backgroundColor = projCollection["backgroundColor"];
+	newPreviewLink.appendChild(newPreviewDiv);
+
+	let newPreviewImg = document.createElement("IMG");
+	newPreviewImg.src = projCollection["imageArray"][0];
+	newPreviewDiv.appendChild(newPreviewImg);
+
+	let newPreTitleDiv = document.createElement("DIV");
+	newPreTitleDiv.setAttribute("id","image-descript");
+	newPreTitleDiv.style.backgroundColor = projCollection["backgroundColor"];
+	let newPreTitleText = document.createElement("p");
+	newPreTitleText.setAttribute("id","img-descript");
+	if (projCollection["textColor"] != "") { newPreTitleText.style.color = projCollection["textColor"]; }
+	newPreTitleText.innerText = projCollection["itemTitle"] + " Renders";
+	newPreTitleDiv.appendChild(newPreTitleText);
+	newPreviewDiv.appendChild(newPreTitleDiv);
+
+	displayGridElem.appendChild(newPreviewLink);
+}
+
+function createAniProjectPage(projCollection) {
+	projectTitleElem.innerText = projCollection["itemTitle"] + " Animation";
+	projectDescElem.innerText = projCollection["description"];
+
+	for (let i = 0; i < projCollection["animation"].length; i++) {
+		let newProjDiv = document.createElement("DIV");
+		newProjDiv.setAttribute("id","video-box");
+		newProjDiv.style.backgroundColor = projCollection["backgroundColor"];
+		let newProjVid = document.createElement("VIDEO");
+		let newProjSource = document.createElement("SOURCE");
+		newProjSource.src = projCollection["animation"][i];
+		newProjSource.type = "video/mp4";
+		newProjVid.controls = true;
+		newProjVid.autoplay = true;
+		newProjVid.loop = true;
+		newProjVid.muted = true;
+		newProjVid.appendChild(newProjSource);
+		newProjDiv.appendChild(newProjVid);
+
+		if ((projCollection["animation"].length % 3) == 1 && i == (projCollection["animation"].length - 1)) {
+			newProjDiv.style.gridColumnStart = 2;
+		}
+
+		displayGridElem.appendChild(newProjDiv);
+	}
+}
+
+function createRenProjectPage(projCollection) {
+	projectTitleElem.innerText = projCollection["itemTitle"] + " Renders";
+	projectDescElem.innerText = projCollection["description"];
+
+	for (let i = 0; i < projCollection["imageArray"].length; i++) {
+		let newProjDiv = document.createElement("DIV");
+		newProjDiv.setAttribute("id","image-box");
+		newProjDiv.style.backgroundColor = projCollection["backgroundColor"];
+		let newProjImg = document.createElement("IMG");
+		newProjImg.src = projCollection["imageArray"][i];
+		newProjDiv.appendChild(newProjImg);
+
+		let newProjTitleDiv = document.createElement("DIV");
+		newProjTitleDiv.setAttribute("id","image-descript");
+		newProjTitleDiv.style.backgroundColor = projCollection["backgroundColor"];
+		let newProjTitleText = document.createElement("p");
+		newProjTitleText.setAttribute("id","img-descript");
+		if (projCollection["textColor"] != "") { newProjTitleText.style.color = projCollection["textColor"]; }
+		newProjTitleText.innerText = projCollection["descriptArray"][i];
+		newProjTitleDiv.appendChild(newProjTitleText);
+		newProjDiv.appendChild(newProjTitleDiv);
+
+		if ((projCollection["imageArray"].length % 3) == 1 && i == (projCollection["imageArray"].length - 1)) {
+			newProjDiv.style.gridColumnStart = 2;
+		}
+
+		displayGridElem.appendChild(newProjDiv);
+	}
+}
